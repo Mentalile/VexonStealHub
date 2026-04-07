942cfbd5c24be2b7d3f963bd4ba0875b3273dc84
Your Name <you@example.com>
2026-04-07 15:35:58 +0000

    Add AutoExecute.lua for executor auto-execute folder integration

---
+++ AutoExecute.lua
@@ -0,0 +1,5 @@
+-- Vexon StealHub Auto-Execute Loader
+-- Put this script in your executor's auto-execute folder
+-- It will automatically load the latest Vexon StealHub script when you join any game
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mentalile/VexonStealHub/refs/heads/main/Script.lua",true))()
